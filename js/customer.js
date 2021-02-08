2c5554578ebc12f761574d4b6c87071c36eff650
Shisir GC <shisir.gc44@gmail.com>
2021-02-09 00:56:15 +0545

    updated color schemes

--- js/customer.js
+++ js/customer.js
@@ -1,24 +1,30 @@
-const tableItems=document.querySelector('.table-body');
+const tableItems = document.querySelector(".table-body");
 
-let output='';
-rowInit=0;
-db.collection("customers").get().then(function(querySnapshot) {
-  querySnapshot.forEach(function(doc) {
-    customerData=doc.data();
-    // console.log("Supplier id:",doc.id)
-    supplierId=doc.id;
-    output+=`
+let output = "";
+rowInit = 0;
+db.collection("customers")
+  .get()
+  .then(function (querySnapshot) {
+    querySnapshot.forEach(function (doc) {
+      customerData = doc.data();
+      // console.log("Supplier id:",doc.id)
+      supplierId = doc.id;
+      output += `
       <tr data-id=${supplierId}>
         <td>${++rowInit}</td>
         <td>${customerData.name}</td>
         <td>${customerData.email}</td>
         <td>${customerData.phone_number}</td>
         <td>${customerData.address}</td>
-        <td><a href="map.html" class="text-decoration-none">View</a></td>
-        <td><a href="#" class="btn btn-primary">Explore</a></td>
+        <td><button class="text-decoration-none btn bg-primary text-white  btnMap">View</button></td>
+        <td>
+          <a href="#" class="btn bg-medium-light text-white ">Explore</a>
+          <a href="#" class="btn bg-success text-white ">Update</a>
+          <a href="#" class="btn bg-delete text-white ">Delete</a>
+        </td>
       </tr>
     `;
-    localStorage.setItem('customerCount',rowInit);
-    tableItems.innerHTML=output;
+      localStorage.setItem("customerCount", rowInit);
+      tableItems.innerHTML = output;
+    });
   });
-});
--- js/supplier.js
+++ js/supplier.js
@@ -16,11 +16,11 @@ function getSuppliers() {
           <td>${supplierData.email}</td>
           <td>${supplierData.phone_number}</td>
           <td>${supplierData.tanker_count}</td>
-          <td><a href="map.html" class="text-decoration-none" onclick="getLatitudeLongitude(supplierData.latitude,supplierData.longitude)">View</a></td>
-          <td><a class="btn btn-info text-white explore-button" href="#tanker-section">Explore</a></td>
+          <td><a href="map.html" class="text-decoration-none btn bg-primary text-white btnMap" onclick="getLatitudeLongitude(supplierData.latitude,supplierData.longitude)">View</a></td>
+          <td><a class="btn explore-button bg-medium-light text-white" href="#tanker-section">Explore</a><a class="btn explore-button bg-success text-white ml-1" href="#tanker-section">Update</a><a class="btn explore-button bg-delete ml-1 text-white" href="#tanker-section">Delete</a></td>
         </tr>
       `;
-        localStorage.setItem('supplierCount',rowInit);
+        localStorage.setItem("supplierCount", rowInit);
         tableItems.innerHTML = output;
         document
           .querySelector(".explore-button")
@@ -39,7 +39,7 @@ function getLatitudeLongitude(latitude, longitude) {
   localStorage.setItem("longitude", longitude);
 }
 
-function exploreTankers(){
+function exploreTankers() {
   console.log("tankeres");
 }
 
